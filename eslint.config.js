7ef2f38b14130579410b679c1d7c121a9654ded4
spi9325 <sanketinamdar72@gmail.com>
2025-07-31 01:52:57 +0530

    test

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
+
 export default tseslint.config([
   globalIgnores(['dist']),
   {
@@ -20,4 +21,4 @@ export default tseslint.config([
       globals: globals.browser,
     },
   },
-])
+]);
